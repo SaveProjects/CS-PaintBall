62b1eb66a26b90bfdc1d35e9c6f74a2b758b0608
EdmineOfficialGitHub <edminerepository@gmail.com>
2023-08-23 01:27:36 +0200

    first commit

--- src/fr/edminecoreteam/cspaintball/Core.java
+++ src/fr/edminecoreteam/cspaintball/Core.java
@@ -7,17 +7,35 @@ public class Core extends JavaPlugin
 {
 
     private static Core instance;
+    private State state;
+
+    public MySQL database;
 
     private static Plugin plugin;
 
     @Override
     public void onEnable() {
+        instance = this;
         saveDefaultConfig();
+        MySQLConnect();
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    private void MySQLConnect() {
+        instance = this;
+        (this.database = new MySQL("jdbc:mysql://", this.getConfig().getString("mysql.host"), this.getConfig().getString("mysql.database"), this.getConfig().getString("mysql.user"), this.getConfig().getString("mysql.password"))).connexion();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public boolean isState(State state) {
+        return this.state == state;
     }
 
     public static Core getInstance() { return Core.instance; }
