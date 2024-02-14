d848c7e32910fbb0467738353bf006aaaac06347
EdmineOfficialGitHub <edminerepository@gmail.com>
2024-02-14 04:45:22 +0100

    .

--- src/main/java/fr/edminecoreteam/cspaintball/utils/minecraft/holograms/HologramsBuilder.java
+++ src/main/java/fr/edminecoreteam/cspaintball/utils/minecraft/holograms/HologramsBuilder.java
@@ -1,10 +1,14 @@
 package fr.edminecoreteam.cspaintball.utils.minecraft.holograms;
 
 import fr.edminecoreteam.cspaintball.Core;
+import net.minecraft.server.v1_8_R3.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 
 import java.util.*;
 
@@ -12,10 +16,16 @@ public class HologramsBuilder
 {
     private final static Core core = Core.getInstance();
     private final HashMap<String, List<ArmorStand>> armorStands = new HashMap<>();
+    private final HashMap<Player, HashMap<String, List<EntityArmorStand>>> armorStandsNMS = new HashMap<>();
+
+    public HologramsBuilder()
+    {
+        //To Main
+    }
 
     public void createBukkitHologram(String id, List<String> entry, Location location)
     {
-        List<ArmorStand> aList = new ArrayList<ArmorStand>();
+        List<ArmorStand> aList = new ArrayList<>();
         for (String en : entry)
         {
             Location loc = new Location(location.getWorld(), location.getX(), location.getY() - 0.3f, location.getZ());
@@ -29,7 +39,7 @@ public class HologramsBuilder
             aList.add(armorStand);
         }
         armorStands.put(id, aList);
-        System.out.println("EDMINE-API: Load Hologram with ID: " + id);
+        System.out.println("EDMINE-API: Load Hologram with ID: " + id + " | and loads " + entry.size() + " entities.");
     }
 
     public void removeBukkitHolgram(String id)
@@ -39,12 +49,14 @@ public class HologramsBuilder
             String key = en.getKey();
             if (key.equalsIgnoreCase(id))
             {
+                int entities = 0;
                 for (ArmorStand stand : en.getValue())
                 {
                     stand.remove();
+                    entities++;
                 }
                 armorStands.remove(id);
-                System.out.println("EDMINE-API: Remove Hologram with ID: " + id);
+                System.out.println("EDMINE-API: Remove Hologram with ID: " + id + " | and removed " + entities + " entities.");
                 return;
             }
         }
@@ -84,6 +96,57 @@ public class HologramsBuilder
                 }
                 en.getValue().remove(getLine);
                 System.out.println("EDMINE-API: Remove Hologram Line (" + getLine + ") with ID: " + id);
+            }
+        }
+    }
+
+    public void createPacketHologram(Player p, String id, List<String> entry, Location location)
+    {
+        List<EntityArmorStand> aList = new ArrayList<>();
+        for (String en : entry)
+        {
+            WorldServer ws = ((CraftWorld)location.getWorld()).getHandle();
+            EntityArmorStand nmsStand = new EntityArmorStand(ws);
+            nmsStand.setLocation(location.getX(), location.getY() -0.3f, location.getZ(), 0.0f, 0.0f);
+            nmsStand.setInvisible(true);
+            nmsStand.setSmall(true);
+            nmsStand.setCustomName(en);
+            nmsStand.setCustomNameVisible(true);
+            nmsStand.setGravity(false);
+            aList.add(nmsStand);
+            PacketPlayOutSpawnEntityLiving sendPacket = new PacketPlayOutSpawnEntityLiving((EntityLiving) nmsStand);
+            ((CraftPlayer)p).getHandle().playerConnection.sendPacket(sendPacket);
+        }
+        HashMap<String, List<EntityArmorStand>> innerMap = new HashMap<>();
+        innerMap.put(id, aList);
+        armorStandsNMS.put(p, innerMap);
+        System.out.println("EDMINE-API: Load Packet Hologram for player (" + p.getName() + ") with ID: " + id + " | and send " + entry.size() + " packets.");
+    }
+
+    public void removePacketHologram(Player p, String id)
+    {
+        for (Map.Entry<Player, HashMap<String, List<EntityArmorStand>>> en : armorStandsNMS.entrySet())
+        {
+            Player key = en.getKey();
+            if (key == p)
+            {
+                HashMap<String, List<EntityArmorStand>> innerMap = en.getValue();
+                for (Map.Entry<String, List<EntityArmorStand>> entry : innerMap.entrySet())
+                {
+                    if (entry.getKey().equalsIgnoreCase(id))
+                    {
+                        int packets = 0;
+                        for (EntityArmorStand entityArmorStands : entry.getValue())
+                        {
+                            PacketPlayOutEntityDestroy sendPacket = new PacketPlayOutEntityDestroy(entityArmorStands.getId());
+                            ((CraftPlayer) p).getHandle().playerConnection.sendPacket(sendPacket);
+                            packets++;
+                        }
+                        armorStandsNMS.remove(p, innerMap);
+                        System.out.println("EDMINE-API: Remove Packet Hologram for player (" + p.getName() + ") with ID: " + id + " | and removed " + packets + " packets.");
+                        return;
+                    }
+                }
             }
         }
     }
